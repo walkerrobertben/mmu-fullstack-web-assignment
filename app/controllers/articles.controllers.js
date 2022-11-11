e0c1cab96f495c9f192f1bc51feda77f70624486
Ben Walker <63571454+walkerrobertben@users.noreply.github.com>
2022-11-11 12:35:45 +0000

    implemented article addNew and getOne

--- app/controllers/articles.controllers.js
+++ app/controllers/articles.controllers.js
@@ -12,11 +12,45 @@ self.getAll = (req, res) => {
 }
 
 self.create = (req, res) => {
-    return res.sendStatus(500);
+
+    { //validation
+        const schema = Joi.object({
+            "title": Joi.string().required(),
+            "author": Joi.string().required(),
+            "article_text": Joi.string().required()
+        });
+
+        const validation = schema.validate(req.body);
+
+        if (validation.error) {
+            return res.status(400).send(error.details[0].message);
+        }
+    }
+
+    const article = Object.assign({}, req.body);
+
+    model.addNew(article).then((article_id) => {
+        res.status(201).send({article_id: article_id});
+    }).catch((error) => {
+        console.error(error);
+        res.sendStatus(500);
+    });
 }
 
 self.getOne = (req, res) => {
-    return res.sendStatus(500);
+
+    const article_id = parseInt(req.params.article_id);
+
+    model.getOne(article_id).then((article) => {
+        if (row != null) {
+            res.status(200).send(article);
+        } else {
+            res.sendStatus(404);    
+        }
+    }).catch((error) => {
+        console.error(error);
+        res.sendStatus(500);
+    });
 }
 
 self.updateArticle = (req, res) => {
--- app/models/articles.models.js
+++ app/models/articles.models.js
@@ -2,7 +2,7 @@ const self = {};
 
 const db = require("../../database");
 
-function DbRowToArticle(row) {
+function DBRowToArticle(row) {
     return {
         article_id: row.article_id,
         title: row.title,
@@ -25,7 +25,7 @@ self.getAll = () => {
             if(error) {
                 console.error("Row error" + error);
             } else {
-                articles.push(RowToArticle(row));
+                articles.push(DBRowToArticle(row));
             }
         }, (error, rowCount) => {
             if (error) {
@@ -34,6 +34,44 @@ self.getAll = () => {
                 resolve(articles);
             }
         });
+    });
+}
+
+self.addNew = (article) => {
+    return new Promise((resolve, reject) => {
+        const date = Date.now();
+
+        const query = "INSERT INTO articles (title, author, date_published, date_edited, article_text, created_by) VALUES(?,?,?,?,?,?)";
+        const params = [article.title, article.author, date, date, article.article_text, 1];
+    
+        db.run(query, params, function(error) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(this.lastID);
+            }
+        });
+    });
+}
+
+self.getOne = (article_id) => {
+    return new Promise((resolve, reject) => {
+
+        const query = "SELECT * FROM articles WHERE article_id=?";
+        const params = [article_id];
+
+        db.get(query, params, (error, row) => {
+            if (error) {
+                reject(error);
+            } else {
+                if (row != null) {
+                    resolve(DBRowToArticle(row));
+                } else {
+                    resolve(null);
+                }
+            }
+        });
+
     });
 }
 
