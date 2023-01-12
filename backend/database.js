ff16e386269b4d86aa24d473dabfb8b346a302d0
Ben Walker <63571454+walkerrobertben@users.noreply.github.com>
2023-01-12 17:28:55 +0000

    Refactored into backend folder

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose()
 const crypto = require("crypto")
 
-const DBSOURCE = "db.sqlite"
+const DBSOURCE = "backend/db.sqlite"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
