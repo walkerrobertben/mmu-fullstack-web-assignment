30000d81bc9475ca9d0214fcca09e16a57ce8528
Ben Walker <63571454+walkerrobertben@users.noreply.github.com>
2023-01-17 15:45:15 +0000

    Designed article edit page

--- frontend/src/views/pages/Article/Edit.vue
+++ frontend/src/views/pages/Article/Edit.vue
@@ -5,12 +5,42 @@
             :back="true"
             :links="[
                 {to: '/articles', name: 'Articles'},
-                {to: `/article/${article_id}`, name: `${article.title}`},
+                {to: `/article/${article_id}`, name: `${nice_title}`},
                 {name: 'Edit Article'}
             ]"
         />
 
         <Title b-text="Edit Article"></Title>
+
+        <n-card style="margin-bottom: 1.5rem">
+            <n-form>
+                <n-form-item label="Title">
+                    <n-input v-model:value="article.title" :placeholder="nice_title" ></n-input>
+                </n-form-item>
+
+                <n-form-item label="Author Name">
+                    <n-input v-model:value="article.author" :placeholder="nice_author"></n-input>
+                </n-form-item>
+
+                <n-form-item label="Body">
+                    <n-input v-model:value="article.article_text" :placeholder="nice_body" type="textarea" :autosize="{minRows: 10}"></n-input>
+                </n-form-item>
+            </n-form>
+        </n-card>
+
+        <n-card style="margin-bottom: 1.5rem">
+            <n-text style="display: inline-block; line-height: 1.25; min-height: 20px; padding-left: 2px; padding-bottom: 6px;">Visiblity</n-text>
+            
+            <n-space align="center" :size="12">
+                <n-button-group>
+                    <n-button secondary :type="is_private ? 'info' : 'default'" @click="() => {setVisibility(false)}">Private</n-button>
+                    <n-button secondary :type="is_private ? 'default' : 'primary'" @click="() => {setVisibility(true)}">Public</n-button>
+                </n-button-group>
+                <n-tag :type="is_private ? 'info' : 'primary'" v-text="is_private ? 'Only you can see this article' : 'Everyone can see this article'"></n-tag>
+            </n-space>
+        </n-card>
+
+        <n-button type="primary" style="margin-left:">Save changes</n-button>
 
     </div>
 </template>
@@ -22,16 +52,35 @@
 </style>
 
 <script>
+
+import { computed } from "vue"
+
 import { article_service } from "../../../services/article.service"
 
 import Subnav from "../../components/navigation/subnav.vue"
 import Title from "../../components/universal/title.vue"
+
+function setVisibility(isPublic) {
+    this.is_private = !isPublic;
+}
 
 export default {
     data() {
         return {
             article_id: -1,
             article: {},
+
+            is_private: false,
+
+            nice_title: computed(() => {
+                return this.article.title == "" ? "My first article" : this.article.title;
+            }),
+            nice_author: computed(() => {
+                return this.article.author == "" ? "John Doe" : this.article.author;
+            }),
+            nice_body: computed(() => {
+                return `Hi, my name is ${this.nice_author} and this is my first article.`
+            }),
         }
     },
     mounted() {
@@ -49,6 +98,8 @@ export default {
             console.error(error);
         });
     },
+
+    methods: {setVisibility},
     components: {Subnav, Title}
 }
 </script>
