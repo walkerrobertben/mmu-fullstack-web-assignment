7e0f5dae22446e430f6e4f2a44468ecc5d033a5f
Ben Walker <63571454+walkerrobertben@users.noreply.github.com>
2023-01-17 18:15:05 +0000

    Made edit button an actual button (instead of tag)

--- frontend/src/views/components/articles/tags.vue
+++ frontend/src/views/components/articles/tags.vue
@@ -2,18 +2,11 @@
     <n-space justify="right" :size="8">
         <n-tag v-if="bShowWrote" type="warning">Your wrote this article</n-tag>
         <n-tag :type="bIsPrivate ? 'info' : 'primary'">{{ bIsPrivate ? 'Private' : 'Public' }}</n-tag>
-        <n-tag class="b-edit-button" @click="editArticle">Edit</n-tag>
+        <n-button size="small" @click="editArticle">Edit</n-button>
     </n-space>
 </template>
 
-<style scoped>
-.n-tag.b-edit-button {
-    cursor: pointer;
-}
-</style>
-
 <script>
-
 function editArticle() {
     this.$router.push(`/article/${this.bArticleId}/edit`);
 }
